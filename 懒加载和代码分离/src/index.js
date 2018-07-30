75d54bec8a7282d80e26f4ff57de8c0a272bbfc0
zyh <715181149@qq.com>
2018-07-30 19:26:33 +0800

    懒加载和代码分离

---
+++ 懒加载和代码分离/src/index.js
@@ -0,0 +1,44 @@
+import printMe from "./print.js";
+import _ from "lodash";
+import { cube } from "./math.js";
+
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
+}
+
+function component() {
+  // var element = document.createElement("pre");
+  // var btn = document.createElement("button");
+
+  // Lodash, now imported by this script
+
+  // btn.innerHTML = "Click me and check the console!";
+  // btn.onclick = printMe;
+
+  // element.appendChild(btn);
+
+  // element.innerHTML = ["Hello webpack!", "5 cubed is equal to "  cube(5)].join(
+  //   "\n\n"
+  // );
+
+  var button = document.createElement("button");
+  var br = document.createElement("br");
+
+  button.innerHTML = "Click me and look at the console!";
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e =>
+    import(/* webpackChunkName: "print" */ "./print").then(module => {
+      var print = module.default;
+
+      print();
+    });
+
+  return element;
+}
+
+document.body.appendChild(component());
