faf3db3930f7cc70d05f96f3c7d05351442a0763
zyh <715181149@qq.com>
2018-07-30 18:11:15 +0800

    tree sharking

---
+++ tree sharking/src/index.js
@@ -0,0 +1,38 @@
+
+import printMe from './print.js';
+import './styles.css';
+import { cube } from './math.js';
+
+
+function component() {
+
+  var element = document.createElement('pre');
+  var btn = document.createElement('button');
+
+  // Lodash, now imported by this script
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
+  return element;
+}
+
+let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element);
+
+if (module.hot) {
+   module.hot.accept('./print.js', function() {
+     console.log('Accepting the updated printMe module!');
+     printMe();
+
+     document.body.removeChild(element);
+     element = component(); // 重新渲染页面后，component 更新 click 事件处理
+     document.body.appendChild(element);
+   })
+ }
