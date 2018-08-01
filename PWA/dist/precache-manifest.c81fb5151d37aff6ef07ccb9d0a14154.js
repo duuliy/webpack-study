c2d22a85abe74dc4547283bade9fc92b691622d5
zyh <715181149@qq.com>
2018-08-01 19:29:49 +0800

    PWA

---
+++ PWA/dist/precache-manifest.c81fb5151d37aff6ef07ccb9d0a14154.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ff4e7eb0310a04ec24",
+    "url": "print.bundle.js"
+  },
+  {
+    "revision": "4e051cefff5301973505fa2026506a82",
+    "url": "index.html"
+  },
+  {
+    "revision": "8327160cec7306768367",
+    "url": "app.bundle.js"
+  }
+];
